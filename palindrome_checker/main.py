80725a0d6741ea84ff026db0f2b8d3111bff0125
szafnerzs <szafnerzsolt2k06@gmail.com>
2024-05-27 09:35:48 +0200

    init

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -1,4 +1,25 @@
 with open('./input.txt', 'r') as f:
   input = f.read()
 
-print(input)
+
+item_list = input.splitlines()
+
+results = {}
+for item in item_list:
+
+	#alfanumerikus karakterek szűrése & számlálása
+	alphanum_chars = ''
+	for char in item:
+		if char.isalnum():
+			alphanum_chars += char.lower()
+	unique_count = len(set(alphanum_chars))
+
+	#Palindróma ellenőrzés
+	if alphanum_chars == alphanum_chars[::-1]:
+		results[item] = ("YES", unique_count)
+	else:
+		results[item] = ("NO", -1)
+
+print(results)
+for palindrome_result, unique_count in results.values():
+	print(f"{palindrome_result}, {unique_count}")
